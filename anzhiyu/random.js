eff04cbf12f8db9e216f19034d58c601b6ea9915
myz66668888 <13007638051@163.com>
2026-01-09 16:11:52 +0800

    Site updated: 2026-01-09 16:11:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3969.html","posts/d4c1.html","posts/1446.html","posts/fb4c.html","posts/d581.html"];function toRandomPost(){
+var posts=["posts/1446.html","posts/d4c1.html","posts/3969.html","posts/fb4c.html","posts/d581.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
