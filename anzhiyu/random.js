eadbdb9d24eef3c68673ac5ff6e3e3989697568d
myz66668888 <13007638051@163.com>
2026-01-12 15:04:23 +0800

    Site updated: 2026-01-12 15:04:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/d581.html","posts/3969.html","posts/1446.html","posts/d4c1.html","posts/fb4c.html","posts/d581.html","posts/536e.html"];function toRandomPost(){
+var posts=["posts/d581.html","posts/d4c1.html","posts/1446.html","posts/3969.html","posts/fb4c.html","posts/d581.html","posts/536e.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
